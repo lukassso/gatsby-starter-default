f39d15ff2ef7902ab2ca0fe478651d8a4f0aeb0c
lukasso <lukasz.koladko@gmail.com>
2019-11-19 09:59:06 +0100

    migrate for noturalOrganic

---
+++ old/webpack.config.js
@@ -0,0 +1,8 @@
+  // Check if we are on dev or prod mode
+const env = process.env.NODE_ENV;
+
+module.exports = env => {
+  console.log(` Ahoy there! Building with ${env} mode using ./webpack/webpack.${env}.js 🛠️  `);
+  return require(`./webpack/webpack.${env}.js`);
+};
+
